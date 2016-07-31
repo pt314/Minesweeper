5afb9a913ca4577afdae9f6a4fb56cc6ad6c726f
Pedro Tejada <pedro.tejada.314@gmail.com>
2016-07-31 10:20:22 -0600

    feat: Add difficulty levels

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Classic Minesweeper in Java.
 I really liked the classic Minesweeper game and always wanted to implement my own version, so I'm doint that now, just for fun :)
 
 More details later...
+
+https://en.wikipedia.org/wiki/Microsoft_Minesweeper
+http://www.minesweeper.info/wiki/Windows_Minesweeper
+
+
+Difficulty levels
+-----------------
+
+- Beginner (8x8, 10 mines) -> mine density = 0.15625
+- Intermediate (16x16, 40 mines) -> mine density = 0.15625
+- Expert (24x24, 99 mines) -> mine density = 0.171875
+
--- app/src/pt314/just4fun/minesweeper/Minesweeper.java
+++ app/src/pt314/just4fun/minesweeper/Minesweeper.java
@@ -4,11 +4,13 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.SwingUtilities;
 
 import pt314.just4fun.minesweeper.game.Game;
@@ -20,9 +22,10 @@ public class Minesweeper extends JFrame implements ActionListener {
 
 	private Game game;
 
-	private int numRows = 10;
-	private int numCols = 10;
-	private int numMines = 20;
+	// board size and numer of mines
+	private int numRows;
+	private int numCols;
+	private int numMines;
 
 	// menu bar and menus
     private JMenuBar menuBar;
@@ -30,6 +33,9 @@ public class Minesweeper extends JFrame implements ActionListener {
     
     // game menu items
     private JMenuItem newGameMI;
+    private JRadioButtonMenuItem smallFieldMI;
+    private JRadioButtonMenuItem mediumFieldMI;
+    private JRadioButtonMenuItem largeFieldMI;
     private JMenuItem exitMI;
 
     public Minesweeper() {
@@ -51,9 +57,23 @@ public class Minesweeper extends JFrame implements ActionListener {
         gameMenu = new JMenu("Game");
         menuBar.add(gameMenu);
 
-        newGameMI = new JMenuItem("New");
+        newGameMI = new JMenuItem("New Game");
         newGameMI.addActionListener(this);
         gameMenu.add(newGameMI);
+
+        gameMenu.addSeparator();
+        
+        ButtonGroup group = new ButtonGroup();
+        smallFieldMI = new JRadioButtonMenuItem("Beginner");
+        group.add(smallFieldMI);
+        gameMenu.add(smallFieldMI);
+        mediumFieldMI = new JRadioButtonMenuItem("Intermediate");
+        group.add(mediumFieldMI);
+        gameMenu.add(mediumFieldMI);
+        largeFieldMI = new JRadioButtonMenuItem("Expert");
+        group.add(largeFieldMI);
+        gameMenu.add(largeFieldMI);
+        smallFieldMI.setSelected(true); // default to small
 
         gameMenu.addSeparator();
 
@@ -65,8 +85,30 @@ public class Minesweeper extends JFrame implements ActionListener {
         setJMenuBar(menuBar);
 	}
 	
+	/**
+	 * Sets the size of the board and the number of mines.
+	 */
+	private void setGameDifficulty() {
+		if (smallFieldMI.isSelected()) {
+			numRows = 8;
+			numCols = 8;
+			numMines = 10;
+		}
+		else if (mediumFieldMI.isSelected()) {
+			numRows = 16;
+			numCols = 16;
+			numMines = 40;
+		}
+		else if (largeFieldMI.isSelected()) {
+			numRows = 24;
+			numCols = 24;
+			numMines = 99;
+		}
+	}
+
 	// TODO: set mines after first space is cleared???
 	private void startNewGame() {
+		setGameDifficulty();
 		game = new Game(numRows, numCols, numMines);
 		JPanel board = new MineFieldPanel(game);
 		Container contentPane = getContentPane();
